9cc9db33547a853ff46c75fb33e794f52693ee38
your_name <raul.a.figueroa@hotmail.com>
2020-02-01 11:12:45 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,59 @@
 // Your code here
+let info = [1, 2, 3, -9];
+function mapToNegativize(info) {
+    let arr = [];
+    for (let i = 0; i < info.length; i++){
+        arr.push( -1 * info[i])
+    }
+    return arr
+}
+
+function mapToNoChange(info) {
+    let arr = [];
+    for (let i = 0; i < info.length; i++){
+        arr.push(info[i])
+    }
+    return arr
+}
+  
+function mapToDouble(info) {
+    let arr = [];
+    for (let i = 0; i < info.length; i++){
+        arr.push(2 * info[i])
+    }
+    return arr
+}
+
+function mapToSquare(info) {
+    let arr = [];
+    for (let i = 0; i < info.length; i++){
+        arr.push(info[i] * info[i])
+    }
+    return arr
+}
+
+function reduceToTotal(info, start = 0){
+    let total = start;
+    for (let i = 0; i < info.length; i++){
+        total = total + info[i]
+    }
+    return total 
+}
+
+function reduceToAllTrue(info){
+    for (let i = 0; i < info.length; i++){
+        if (!info[i])
+            return false
+    }
+    return true
+}
+
+function reduceToAnyTrue(info){
+    for (let i = 0; i < info.length; i++){
+        if (info[i])
+            return true
+    }
+    return false
+
+
+}
